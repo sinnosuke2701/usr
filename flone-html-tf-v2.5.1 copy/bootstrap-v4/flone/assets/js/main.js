4f2a220b743c8ac47dac33dc2c46bf4c77e3379b
sinnosuke2701 <ehgml1111@naver.com>
2024-08-09 14:08:09 +0900

    usr

--- flone-html-tf-v2.5.1 copy/bootstrap-v4/flone/assets/js/main.js
+++ flone-html-tf-v2.5.1 copy/bootstrap-v4/flone/assets/js/main.js
@@ -481,15 +481,14 @@
     $(function() {
         sliderrange.slider({
             range: true,
-            min: 16,
-            max: 400,
+            max: 100000,
             values: [0, 300],
             slide: function(event, ui) {
-                amountprice.val("$" + ui.values[0] + " - $" + ui.values[1]);
+                amountprice.val("₩" + ui.values[0] + " - ₩" + ui.values[1]);
             }
         });
-        amountprice.val("$" + sliderrange.slider("values", 0) +
-            " - $" + sliderrange.slider("values", 1));
+        amountprice.val("₩" + sliderrange.slider("values", 0) +
+            " - ₩" + sliderrange.slider("values", 1));
     });
     
     
